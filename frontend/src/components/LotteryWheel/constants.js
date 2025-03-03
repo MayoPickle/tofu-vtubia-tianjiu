c6311ab88c1aeff073da9a1517883414447723c8
Boyang Yu <me@boyangyu.cn>
2025-03-03 19:34:07 +0000

    feat: update LotteryWheel prize constants with new names and images

--- frontend/src/components/LotteryWheel/constants.js
+++ frontend/src/components/LotteryWheel/constants.js
@@ -1,7 +1,7 @@
 // constants.js
 export const defaultPrizes = [
-    { name: '默认一等奖', probability: 0.2, image: 'https://via.placeholder.com/300?text=默认一等奖' },
-    { name: '默认二等奖', probability: 0.3, image: 'https://via.placeholder.com/300?text=默认二等奖' },
-    { name: '默认三等奖', probability: 0.5, image: 'https://via.placeholder.com/300?text=默认三等奖' },
+    { name: '旺旺', probability: 0.2, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTN_FxYRgRP9Diku5wUhgg3PiciL5CJBFu46Q&s' },
+    { name: '袜袜', probability: 0.3, image: 'https://cdn.yamibuy.net/itemdescription/c0f98c73b07e3b746419ece26db18fcb_0x0.webp' },
+    { name: '狗狗', probability: 0.5, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeCBPTS5r3v6v-0K0YWhNp4XAHoCHAdW1nuQ&s' },
   ];
   
